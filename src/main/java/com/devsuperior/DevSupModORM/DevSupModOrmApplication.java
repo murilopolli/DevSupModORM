a6b30c4ab3022cfce446713a719649581e84adbf
Murilo Polli <murilopolli@gmail.com>
2025-03-25 22:00:54 -0300

    first commit

---
+++ src/main/java/com/devsuperior/DevSupModORM/DevSupModOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DevSupModORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSupModOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSupModOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/DevSupModORM/DevSupModOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DevSupModORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSupModOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
